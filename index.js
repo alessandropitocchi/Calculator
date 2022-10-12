cefd4b06ce915faae070b869be4698d9aea3dc43
Alessandro Pitocchi <Alessandro.Pitocchi@elmec.it>
2022-10-12 12:23:51 +0200

    Added equal, point , delete and kandled keyboard

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const operator = document.querySelectorAll('.operator')
 const result = document.querySelector('.result')
 const screen = document.querySelector('.screen')
 const clearButton = document.querySelector('.clearBtn')
+const equalBtn = document.querySelector('.equalBtn')
+const deleteBtn = document.querySelector('.deleteBtn')
+const pointBtn = document.querySelector('.pointBtn')
 
 let firstValue = ''
 let secondValue = ''
-let op = ''
+let op = null
 let reset = false
 
 clearButton.addEventListener('click', fullResetScreen);
+equalBtn.addEventListener('click', calc)
+deleteBtn.addEventListener('click', deleteNumber)
+pointBtn.addEventListener('click', addPoint)
+document.addEventListener('keydown', keyboardInput)
 
 numbers.forEach((number) => {
     number.addEventListener('click', (e) => appendNumber(e.target.getAttribute('value')))
@@ -27,26 +34,63 @@ function appendNumber(number){
 }
 
 function setValue(operator){
-    if(op !== '' ) calc()
+    if(op !== null ) calc()
     op = operator 
     firstValue = result.textContent
-    screen.textContent = `${firstValue} ${op}`
+    if (op !== null){
+        screen.textContent = `${firstValue} ${op}`
+    } else {
+        screen.textContent = ''
+    }
     reset = true
 }
 
+function round(n){
+    return Math.round(n * 1000) / 1000
+}
+
+function addPoint(){
+    if (reset) resetScreen()
+    if (result.textContent === '') result.textContent = '0'
+    if (result.textContent.includes('.')) return
+    result.textContent += '.'
+}
+
+function deleteNumber(){
+    result.textContent = result.textContent.slice(0, -1)
+}
+
 function calc(){
+    if (op === '/' && result.textContent === '0'){
+        alert('ERROR!')
+        fullResetScreen()
+        return
+    }
     secondValue = result.textContent
-    result.textContent = operate(op, firstValue, secondValue)
-    op = ''
+    result.textContent = round(operate(op, firstValue, secondValue))
+    op = null
 }
 
 function resetScreen(){
     result.textContent = ''
+    reset = false
 }
 
 function fullResetScreen(){
     result.textContent = '0'
     screen.textContent = '' 
+    firstValue = ''
+    secondValue = ''
+    op = null
+}
+
+function keyboardInput(e){
+    if(e.key == '/' || e.key == '-' || e.key == '+' || e.key == '*' ) setValue(e.key)
+    if(e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if(e.key == '=' || e.key == 'Enter') calc()
+    if(e.key == '.' || e.key == ',') addPoint()
+    if(e.key == 'F2') fullResetScreen()
+    console.log(e.key)
 }
 
 function add(x, y){
